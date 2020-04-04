87f8d4a332b7de3f6b0046aeb11f9a8347cc0991
Dhiraj Patra <dhiraj.patra@gmail.com>
2020-04-04 10:04:59 +0530

    updated with postgresql connection

--- djangoapi/settings.py
+++ djangoapi/settings.py
@@ -81,8 +81,14 @@ WSGI_APPLICATION = 'djangoapi.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'testdb',
+        'USER': 'pgroot',
+        'PASSWORD': 'pgsqladmin',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
